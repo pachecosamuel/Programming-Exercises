59cd6a0d904dc24370f6bb3fc8818e5e9b11175c
Samuel Pacheco <87822546+pachecosamuel@users.noreply.github.com>
2022-01-08 15:55:13 -0300

    Data treatment for calculation

---
+++ beecrowd exercises/beecrowd-1010.py
@@ -0,0 +1,9 @@
+l1 = input().split(" ")
+l2 = input().split(" ")
+
+cod1, qtd1, preco1 = l1
+cod2, qtd2, preco2 = l2
+
+total = (int(qtd1) * float(preco1)) + (int(qtd2)*float(preco2))
+
+print(f'VALOR A PAGAR: R$ {total:.2f}')
