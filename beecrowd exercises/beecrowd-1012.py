6286406afc17e0701c48849fe0e0b4a4cd64a5e1
Samuel Pacheco <87822546+pachecosamuel@users.noreply.github.com>
2022-01-08 16:32:52 -0300

    Calculation of areas of △, ○, trapezoid, ▀ and ▂.

---
+++ beecrowd exercises/beecrowd-1012.py
@@ -0,0 +1,10 @@
+values = input().split()
+a = float(values[0])
+b = float(values[1])
+c = float(values[2])
+
+print(f'TRIANGULO: {((a*c)/2):.3f}')
+print(f'CIRCULO: {(3.14159*(c**2)):.3f}')
+print(f'TRAPEZIO: {(((a+b) * c) / 2):.3f}')
+print(f'QUADRADO: {(b**2):.3f}')
+print(f'RETANGULO: {(a*b):.3f}')
