008151e0602dfa755e8426cfa66e544ba9c9cb5e
Samuel Pacheco <87822546+pachecosamuel@users.noreply.github.com>
2022-01-10 21:11:12 -0300

    Calculate the bigger number in a sequence

---
+++ beecrowd exercises/beecrowd-1013.py
@@ -0,0 +1,15 @@
+values = input().split(" ")
+a = int(values[0])
+b = int(values[1])
+c = int(values[2])
+
+maior = a
+
+if maior < b and b > c:
+    maior = b
+    print(f'{maior} eh o maior')
+elif maior < c:
+    maior = c
+    print(f'{maior} eh o maior')
+else:
+    print(f'{maior} eh o maior')
