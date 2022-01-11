182c2c4738dc1a01e7dee79dd5160eb42359acd6
Samuel Pacheco <87822546+pachecosamuel@users.noreply.github.com>
2022-01-11 11:31:51 -0300

    Time conversion
    Read an integer value, which is the duration time in seconds of a given event in a factory, and enter it expressed in the format hours:minutes:seconds.

---
+++ beecrowd exercises/beecrowd-1019.py
@@ -0,0 +1,10 @@
+time = eval(input())
+qtdtime = [3600, 60, 1]
+result = []
+
+for i in qtdtime:
+    qtd = time // i
+    result.append(str(qtd))
+    time -= qtd * i
+
+print(f'{result[0]}:{result[1]}:{result[2]}')
