44612b4a93e24cc1cc938fb82bf94f577cb27cab
Samuel Pacheco <87822546+pachecosamuel@users.noreply.github.com>
2022-03-08 10:51:13 -0300

    Above Main Diagonal
    Read an uppercase character, which indicates an operation that must be performed, and an array M[12][12]. Then, calculate and display the sum or average considering only those elements that are above the main diagonal of the matrix, as illustrated below (green area).
    
    
    
    
    Input
    The first input line contains a single Capital O character ('S' or 'M'), indicating the operation (Sum or Average) to be performed with the elements of the matrix. Following are the 144 floating point values that make up the array.
    
    Exit
    Print the requested result (the sum or average), with 1 place after the decimal point.
    
    Input example
    s
    1.0
    0.0
    -3.5
    2.5
    4.1
    ...
    
    Output example
    12.6

---
+++ beecrowd exercises/beecrowd-1183.py
@@ -0,0 +1,24 @@
+op = input()
+matriz = []
+
+for i in range(12):
+    linha = []
+    for j in range(12):
+        linha.append(float(input()))
+    matriz.append(linha)
+
+soma = 0
+cont = 1
+c2 = 0
+
+for k in range(11):
+    for L in range(cont, 12):
+        soma += matriz[k][L]
+        c2 += 1
+    cont += 1
+
+if op == 'S':
+    print(f"{soma:.1f}")
+elif op == 'M':
+    print(f"{soma/c2:.1f}")
+
